4a6a4ac6af610023056a27fedc6ae2d21173e809
Rohit <rohitkamble10792@gmail.com>
2022-05-22 16:15:26 +0530

    Printed Prime Factors of Number N

--- src/com/bridgelabz/factors/Factors.java
+++ src/com/bridgelabz/factors/Factors.java
@@ -1,8 +1,31 @@
 package com.bridgelabz.factors;
 
+import com.bridgelabz.util.Utility;
+
 public class Factors {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Factors Program");
+        // INITIALIZATION
+        int n;
+        Utility u = new Utility();
+
+        // INPUT DATA
+        System.out.print("Enter a Number : ");
+        n = u.inputInteger();
+
+        // COMPUTATION
+        System.out.print("The Prime Factors of " + n + " are : ");
+
+        int i = 2;
+        while (n > 1) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                n = n / i;
+            } else
+                i++;
+        }
+        System.out.println(" ");
     }
 }
+
+
